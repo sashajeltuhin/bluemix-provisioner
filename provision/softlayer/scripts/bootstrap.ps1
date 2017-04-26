e4c9796e6b88b347ea82f996150ce7e5171d4c4c
Sasha <sjeltuhin@apprenda.com>
2017-04-26 17:13:36 -0400

    bootstrap script clean-up

--- provision/softlayer/scripts/bootstrap.ps1
+++ provision/softlayer/scripts/bootstrap.ps1
@@ -1,7 +1,13 @@
-$domainPass = "@ppr3nda" \n\r
-$LocalAdmin = "Administrator" \n\r
-$objUser = [ADSI]"WinNT://localhost/$($LocalAdmin), user" \n\r
-$objUser.psbase.Invoke("SetPassword", $domainPass) \n\r
-Enable-PSRemoting –force \n\r
-Set-Item wsman:\localhost\client\trustedhosts * \n\r
-Restart-Service WinRM \n\r
+$domainPass = "@ppr3nda"
+
+$LocalAdmin = "Administrator"
+
+$objUser = [ADSI]"WinNT://localhost/$($LocalAdmin), user"
+
+$objUser.psbase.Invoke("SetPassword", $domainPass)
+
+Enable-PSRemoting -Force
+
+Set-Item wsman:\localhost\client\trustedhosts *
+
+Restart-Service WinRM
