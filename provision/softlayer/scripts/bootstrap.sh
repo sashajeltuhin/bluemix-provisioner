ab8fa2b9241417ef843613d0238acac6969184ea
Sasha <sjeltuhin@apprenda.com>
2017-04-27 17:45:46 -0400

    boot for linux

---
+++ provision/softlayer/scripts/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+rootPass="supersecret"
+sed -i \"s/mirrorlist=https/mirrorlist=http/\" /etc/yum.repos.d/epel.repo
+yum check-update
+yum -y install wget libcgroup cifs-utils nano openssh-clients libcgroup-tools unzip iptables-services net-tools bind bind-utils
+service cgconfig start
+echo "root:$rootPass" | chpasswd
+echo "Updating sshd to allow root login via ssh"
+sed -i 's/#\?\(RSAAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 yes/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
+service sshd restart
+echo "Install git"
+yum install -y git
+echo "Installing Docker"
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+yum install -y http://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.11.1-1.el7.centos.x86_64.rpm
+systemctl daemon-reload
+systemctl start docker
+systemctl enable docker
