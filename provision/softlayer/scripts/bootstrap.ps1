84672b3e6cec640603b5ec92d91247d56009eaac
Sasha <sjeltuhin@apprenda.com>
2017-04-27 11:24:17 -0400

    winrm support

--- provision/softlayer/scripts/bootstrap.ps1
+++ provision/softlayer/scripts/bootstrap.ps1
@@ -10,4 +10,8 @@ Enable-PSRemoting -Force
 
 Set-Item wsman:\localhost\client\trustedhosts *
 
+winrm set winrm/config/service/auth @{Basic="true"}
+
+winrm set winrm/config/service @{AllowUnencrypted="true"}
+
 Restart-Service WinRM
